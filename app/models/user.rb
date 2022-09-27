8bc99c85ada242bca99b337688e813c7eaeea029
apple <apple@apples-MacBook-Air.local>
2022-09-27 12:32:50 +0500

    refactor previous code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  ROLES = [:admin, :buyer]       
+
+  ROLES = [:admin, :buyer]
+
   enum role: ROLES
-  scope :buyer_role, -> {where(role: 'buyer')}
 
   has_many :subscriptions
   has_many :plans, through: :subscriptions
