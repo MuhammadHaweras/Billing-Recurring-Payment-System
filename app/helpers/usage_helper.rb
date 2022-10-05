9506cd7c2723ec719feaf38e49f4a2836ff2b3e7
apple <apple@apples-MacBook-Air.local>
2022-10-05 18:07:39 +0500

    add invoice

--- app/helpers/usage_helper.rb
+++ app/helpers/usage_helper.rb
@@ -1,5 +1,5 @@
 module UsageHelper
-  def subscription_feature(plan,feature)
-    Subscription.where(plan_id: plan).where(feature_id: feature.id).first
+  def subscription_feature(user, plan,feature)
+    Subscription.where(user_id: user).where(plan_id: plan).where(feature_id: feature).first
   end
 end
